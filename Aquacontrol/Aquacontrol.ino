1c5a91ff2428836dc95379d19c998f673787ea7c
pablo.garcia <pablo.garcia@bq.com>
2019-09-05 14:56:35 +0200

    Added NTP and MQTT support.

--- Aquacontrol/Aquacontrol.ino
+++ Aquacontrol/Aquacontrol.ino
@@ -1,21 +1,108 @@
 #include "Aquacontrol.h"
-#include <OneWire.h>    // https://www.pjrc.com/teensy/td_libs_OneWire.html
-#include <DS3231M.h>    // https://github.com/SV-Zanshin/DS3231M
+#include <OneWire.h>            // https://www.pjrc.com/teensy/td_libs_OneWire.html
+#include <DallasTemperature.h>  // https://github.com/milesburton/Arduino-Temperature-Control-Library
+#include <DS3231M.h>            // https://github.com/SV-Zanshin/DS3231M
+#include <ESP8266WiFi.h>        // https://arduino-esp8266.readthedocs.io/en/latest/
+#include <WiFiUdp.h>
+#include <NTPClient.h>          // https://github.com/taranais/NTPClient
+#include <PubSubClient.h>       // https://pubsubclient.knolleary.net/
 
-#define   RELAY_LAMP  (4)
-#define   RELAY_AERA  (5)
-#define   RELAY_HEAT  (12)
-#define   RELAY_FILT  (13)
-#define   SENS_THERM  (16)
-#define   SERIAL_SPEED  (115200)
+// Examples: https://techtutorialsx.com/2017/04/09/esp8266-connecting-to-mqtt-broker/
+//           https://randomnerdtutorials.com/esp32-ntp-client-date-time-arduino-ide/
 
-DS3231M_Class DS3231M;    // Instance DS3231M
+OneWire             oneWire(SENS_THERM);    // Instance OneWire bus for Temp Sensor
+DallasTemperature   TempSensor(&oneWire);   // Instance Temp Sensor
+DS3231M_Class       RTClock;                // Instance DS3231M RTC Clock
+WiFiClient          espClient;
+WiFiUDP             ntpUDP;                 // UDP Link for NTP
+NTPClient           timeClient(ntpUDP, "pool.ntp.org", GMTp1_OFF);
+PubSubClient        mqtt_client(espClient);
 
-void setup() {
-  // put your setup code here, to run once:
-  DS3231M.begin();
+
+/* FUNCTIONS DECLARATION   */
+// WiFi_Setup
+// Initiates WiFi Connectivity to predefined SSID
+void WiFi_Setup() {
+  delay(10);
+  Serial.println();
+  Serial.println("INITIALIZING WIFI...................");
+  Serial.print("Connecting to: ");
+  Serial.println(wifi_ssid);
+
+  WiFi.begin(wifi_ssid, wifi_pass);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
+// MQTT_callback
+// Handles incoming messages for the topics subscribed
+void mqtt_callback(char* topic, byte* payload, unsigned int length) {
+ 
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+ 
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+ 
+  Serial.println();
+  Serial.println("-----------------------");
+ 
+}
+
+// MQTT_Setup
+// Initiates MQTT Connectivity to MQTT Broker
+void MQTT_Setup() {
+  Serial.println();
+  Serial.println("INITIALIZING MQTT...................");
+  mqtt_client.setServer(mqtt_server, mqtt_port);
+  mqtt_client.setCallback(mqtt_callback);
+
+  while (!mqtt_client.connected()) {
+    Serial.println("Connecting to MQTT...");
+ 
+    if (mqtt_client.connect("MQTT_Client", mqtt_user, mqtt_pass )) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed with state ");
+      Serial.print(mqtt_client.state());
+      delay(2000);
+    }
+  }
+}
+
+void setResetTime() {
+  String  timeString;
+  // Send reset time
+  timeClient.update();
+  timeString = timeClient.getFormattedDate();
+  Serial.println(timeString);
+  mqtt_client.publish("aquarium/reset", timeString.c_str());
+}
+
+/*  ARDUINO SETUP   */
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(SERIAL_SPEED);
+  RTClock.begin();
+  WiFi_Setup();
+  timeClient.begin();
+  MQTT_Setup();
+  setResetTime();
+}
+
+/*  ARDUINO LOOP   */
 void loop() {
   // put your main code here, to run repeatedly:
 
